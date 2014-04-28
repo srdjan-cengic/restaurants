84afbc17733dbfa08a7f3965f59d02d260aa7045
srdjan <srdjan@olivebh.com>
2014-04-28 20:04:28 +0200

    changes and fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "cengasrle@gmail.com"
+  default from: "restorani.ba@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
