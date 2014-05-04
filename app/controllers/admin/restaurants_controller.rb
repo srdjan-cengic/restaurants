96724bfd98793471a628508a144568b392988832
hare <90dervisevic.haris@gmail.com>
2014-05-04 16:21:19 +0200

    create,delete,update alerts

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -64,7 +64,7 @@ class Admin::RestaurantsController < ApplicationController
   def destroy
     @restaurant.destroy
     respond_to do |format|
-      format.html { redirect_to admin_restaurants_url }
+      format.html { redirect_to admin_restaurants_url, notice: 'Restaurant was successfully deleted.'  }
       format.json { head :no_content }
     end
   end
