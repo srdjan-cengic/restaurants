9829b749d1117b956d23f1c4f103212786fdd141
martinav <martina.vistica@gmail.com>
2014-05-30 01:07:38 +0200

    changes again

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -1,9 +1,9 @@
 RestaurantsBa.Restaurant = DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-   fb_url: DS.attr('string'),
-    image: DS.attr('string'),
-     telephone: DS.attr('string'),
+  fb_url: DS.attr('string'),
+  image: DS.attr('string'),
+  telephone: DS.attr('string'),
 
   nekaFunkcija: function() {
   	return "neka bussines logika!?";
