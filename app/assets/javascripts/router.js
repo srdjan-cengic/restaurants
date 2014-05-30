f67383411ae09d4f2460dd4376ff1d95df3e3560
DzenisaH <dzenisa.husic@gmail.com>
2014-05-30 23:19:40 +0200

    search

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 RestaurantsBa.Router.map(function() {
     // Passing the name of the template => By that "registration.hbs" becomes
     // the template that is render when we visit localhost:3000/#/registration
+   this.route("search");
     this.route("registration");
     this.resource("restaurants", function() {
         this.route("new");
