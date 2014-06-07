bd0865f25b10671970f31489e376185e6aa7b6c3
martinav <martina.vistica@gmail.com>
2014-06-07 18:20:43 +0200

    delete coupon + other changes

--- app/assets/javascripts/controllers/CouponesController.js
+++ app/assets/javascripts/controllers/CouponesController.js
@@ -9,6 +9,23 @@ RestaurantsBa.CouponesController = Ember.ArrayController.extend({
                     console.log("SUCCESS", data);
                 }
             });*/
-        }
+        },
+        deleteCoupon: function(coupone) {
+            if (window.confirm("Are you sure you want to delete this coupon?")) {
+
+                var coupone_id = coupone.get('id');
+
+                $.ajax({
+                    type: "DELETE",
+                    url: "api/coupones/" + coupone_id + "/",
+                    success: function() {
+                        this.transitionToRoute("coupones");
+                    }, 
+                    error: function() {
+                        alert("delete failed");
+                    }             
+                });
+            }
+        },
     }
 });
