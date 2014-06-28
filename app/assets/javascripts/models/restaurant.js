21b2fba691ce9ea72921f0d80ac6363c6ca18733
martinav <martina.vistica@gmail.com>
2014-06-28 21:17:09 +0200

    maps fix oleeeee

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -9,6 +9,10 @@ RestaurantsBa.Restaurant = DS.Model.extend({
   nekaFunkcija: function() {
   	return "neka bussines logika!?";
   },
+  url: function(){
+    var adresa = "https://www.google.com/maps/embed/v1/place?key=AIzaSyC5lp2B_bUPJPLCzOOYwi3-vM442m1PKsA&q=" + this.get("address");
+    return adresa;
+  }.property('address'),
   imagePath: function() {
     return "/assets/"+this.get("image");
   }.property('image')
